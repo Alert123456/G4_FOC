d6c4e6407bd13fedc35e491a256e04a985a89111
xj <923235693@qq.com>
2025-10-06 21:19:31 +0800

    阻塞式ADC电压采样完成，等待整合为DMA连续采样式

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "adc.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
@@ -47,7 +48,9 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
+// 在main.c的main函数之前定义变量
+uint16_t adc_value;
+float voltage;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -92,9 +95,17 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM6_Init();
   MX_USART2_UART_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
+
   HAL_TIM_Base_Start_IT(&htim6);
 
+  // 在main函数的初始化部分之后（/* USER CODE BEGIN 2 */ 之后）
+  // 可以首先执行ADC校准:cite[9]:cite[10]
+  if (HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
   /* USER CODE END 2 */
 
@@ -102,6 +113,20 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  // 启动ADC转换
+	    HAL_ADC_Start(&hadc1);
+	    // 等待转换完成，超时时间设为10ms
+	    if (HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK)
+	    {
+	      // 读取ADC转换值
+	      adc_value = HAL_ADC_GetValue(&hadc1);
+	      // 将ADC值转换为电压值 (假设参考电压为3.3V)
+	      voltage = (float)adc_value * 75.0 * 3.3 / 4095.0 / 3.0;
+	      // 此处可以通过串口打印电压值，或进行其他处理
+	      // printf("ADC Value: %d, Voltage: %.2f V\r\n", adc_value, voltage);
+	    }
+	    HAL_Delay(500); // 延时500ms
+	    HAL_ADC_Stop(&hadc1);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
---
+++ UserApp/Inc/board.h
@@ -0,0 +1,17 @@
+/*
+ * board.h
+ *
+ *  Created on: Oct 6, 2025
+ *      Author: Alert
+ */
+
+#ifndef INC_BOARD_H_
+#define INC_BOARD_H_
+
+#define ADC_REF_V               (float)(3.3)  // ADC参考电压
+#define VBUS_UP_RES             (float)(75.0) // 分压电阻上拉阻值
+#define VBUS_DN_RES             (float)(3.0) // 分压电阻
+#define VBUS_CONVERSION_FACTOR  (float)(ADC_REF_V*(VBUS_UP_RES+VBUS_DN_RES)/VBUS_DN_RES/4095.0) // 计算母线电压的转换系数
+
+
+#endif /* INC_BOARD_H_ */
